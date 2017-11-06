0ef69b80acefaed0a2cfadcb51e7058addf2e30c
Benjie Gillam <benjie@jemjie.com>
2017-11-06 14:59:58 +0000

    Update install instructions

--- src/cli.js
+++ src/cli.js
@@ -16,7 +16,7 @@ if (args[0] === "install") {
   switch (os.platform()) {
     case "darwin":
       console.log(`\
-We've detected you're running OS X. To get things up and running we need to set up the .meh and .dev DNS entries and a port forwarding firewall rule so mehserve takes over port 80 without requiring root privileges.
+We've detected you're running OS X. To get things up and running we need to set up the .meh and .dev DNS entries and a port forwarding firewall rule so mehserve takes over ports 80 and 443 without requiring root privileges.
 
 Please note that you will be responsible for undoing these changes should you wish to uninstall mehserve, and due to these changes you may get delays resolving DNS queries when mehserve is not running.
 
@@ -36,7 +36,7 @@ We recommend running mehserve all the time using daemon manager. And if you foll
       console.log(`\
 We've detected you're running Linux. The following instructions are specifically for Ubuntu (15.10) but you should be able to adjust them to your OS.
 
-We're using dnsmasq to resolve .dev and .meh domains to localhost (127.0.0.1), and iptables to redirect port 12439 locally to port 80 so we don't need to run with root privileges.
+We're using dnsmasq to resolve .dev and .meh domains to localhost (127.0.0.1), and iptables to redirect ports 12439 and 12443 locally to ports 80 and 443 so we don't need to run with root privileges.
 
 Please note that you will be responsible for undoing these changes should you wish to uninstall mehserve. We recommend running mehserve all the time using something like \`pm2\`.
 
@@ -45,6 +45,7 @@ Please note that you will be responsible for undoing these changes should you wi
   echo -e "local=/meh/\\naddress=/meh/127.0.0.1" | sudo tee /etc/dnsmasq.d/meh-tld
   sudo service dnsmasq restart
   sudo iptables -t nat -A OUTPUT -o lo -p tcp --dport 80 -j REDIRECT --to-port 12439
+  sudo iptables -t nat -A OUTPUT -o lo -p tcp --dport 443 -j REDIRECT --to-port 12443
   sudo iptables-save\
 `);
       break;
@@ -56,7 +57,7 @@ mehserve is only tested on OS X and Ubuntu Linux, but you should be able to make
 
   console.log(`\
 
-Please note that you can change the default HTTP port 12439, and DNS port 15353 by providing PORT and DNS_PORT environment varialbes, in which case you will also need to modify resolver and firewall files accordingly.\
+Please note that you can change the default HTTP port 12439, HTTPS port 12443, and DNS port 15353 by providing PORT, SSL_PORT and DNS_PORT environment variables, in which case you will also need to modify resolver and firewall files accordingly.\
 `);
 } else if (args[0] === "run") {
   require("./index");
